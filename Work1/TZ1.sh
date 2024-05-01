6d559b2c872eed88d78774b33f78857f4c48514c
Bananzi <opio-12321@list.ru>
2024-05-02 02:43:49 +0300

    fix: fixing work with empty directory

--- Work1/TZ1.sh
+++ Work1/TZ1.sh
@@ -3,7 +3,7 @@
 copy(){
 #Условием проверяем является ли текущий файл файлом или директорией
 #В случае директории рекурсивно вроваливаемся внутрь
-if [[ -f $1 ]];then
+if [[ -f $1 ]]; then
 	find -name $1 -print0 | while IFS= read -r -d '' path
 	do
 		patht=$path
@@ -36,7 +36,14 @@ input=$1
 output=$2
 cd $input
 echo "Начало процесса копирования файлов"
-for filename1 in *; do
-	copy $filename1
-done
+
+shopt -s nullglob dotglob
+files=(*)
+if [[ !${#files[*]} ]];then
+	for filename in *; do
+		copy $filename
+	done
+fi
+shopt -u nullglob dotglob
+
 echo "Копирование файлов завершено"
